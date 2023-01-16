2f9c532d87605b40f9615aa4cacc87dbf32404bd
samanfathnazarian <saman.nazarian17@gmail.com>
2023-01-15 18:21:39 -0600

    Used useEffect for email validity and login check

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,29 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // creates side effects, also happends everytime the page reloads thus crates an infinite loop of re-rendering the component
+  // const storedUserLoggedInfo = localStorage.getItem('isLoggedIn');
+  // if (storedUserLoggedInfo === '1') {
+  //   setIsLoggedIn(true);
+  // }
+
+  // runs only after component evaluation if the dependencies change, but it runs for the first time when the site starts
+  useEffect(() => {
+    const storedUserLoggedInfo = localStorage.getItem('isLoggedIn');
+    if (storedUserLoggedInfo === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -6,25 +6,54 @@ import Button from '../UI/Button/Button';
 
 const Login = (props) => {
   const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  const [emailIsValid, setEmailIsValid] = useState(); //if invalid highlight red color when blurred
+
   const [enteredPassword, setEnteredPassword] = useState('');
-  const [passwordIsValid, setPasswordIsValid] = useState();
-  const [formIsValid, setFormIsValid] = useState(false);
+  const [passwordIsValid, setPasswordIsValid] = useState(); //if invalid highlight red color when blurred
+
+  const [formIsValid, setFormIsValid] = useState(false); //valid if email has @ & password is longer than 6 char
+
+  // runs after every componenet render cycle, & the first time component mounts / initially created
+  // useEffect(() => {
+  // console.log('EFFECT RUNNING!!!');
+  // });
+
+  // runs at beginning of render cycle
+  // console.log('EFFECT RUNNING!!!');
+
+  // useEffect(() => {
+  //   console.log('HELLO EFFECT!!!');
+
+  //   return () => {
+  //     console.log('BYE EFFECT!!!');
+  //   };
+  // }, []);
+
+  // side effect of user entering data, an action that should be executed in reponse to another action (which is a side effect)
+  useEffect(() => {
+    // debouncing - debounce the user input, don't do something on every key stroke
+    const identifier = setTimeout(() => {
+      console.log('Checking form validity!');
+      setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6);
+    }, 500);
+
+    // clean up func doesn't happen on start but everytime useEffect is used after that it is ran
+    // reset previous timeout, (runs before timeout to clean it up before function runs)
+    // clear last timer before we creating a new one
+    return () => {
+      console.log('CLEANUP!!!!!');
+      clearTimeout(identifier); //stops the timeout from happening if another input is entered
+    };
+  }, [enteredEmail, enteredPassword]); //add the values that you're using as dependencies
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
+    // setFormIsValid(event.target.value.includes('@') && enteredPassword.trim().length > 6);
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
+    // setFormIsValid(event.target.value.trim().length > 6 && enteredEmail.includes('@'));
   };
 
   const validateEmailHandler = () => {
@@ -43,34 +72,16 @@ const Login = (props) => {
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
-          }`}
-        >
+        <div className={`${classes.control} ${emailIsValid === false ? classes.invalid : ''}`}>
           <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={enteredEmail}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
+          <input type="email" id="email" autoComplete="off" value={enteredEmail} onChange={emailChangeHandler} onBlur={validateEmailHandler} />
         </div>
-        <div
-          className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
-          }`}
-        >
+
+        <div className={`${classes.control} ${passwordIsValid === false ? classes.invalid : ''}`}>
           <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={enteredPassword}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
+          <input type="password" id="password" autoComplete="off" value={enteredPassword} onChange={passwordChangeHandler} onBlur={validatePasswordHandler} />
         </div>
+
         <div className={classes.actions}>
           <Button type="submit" className={classes.btn} disabled={!formIsValid}>
             Login
