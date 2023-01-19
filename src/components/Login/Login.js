1c702012a4bdb8869dfb8e246a013d747e110a5e
samanfathnazarian <saman.nazarian17@gmail.com>
2023-01-19 03:57:55 -0600

    Used forward refs to focus the cursor on unfilled login form fields

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, useContext } from 'react';
+import React, { useState, useEffect, useReducer, useContext, useRef } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -46,6 +46,9 @@ const Login = (props) => {
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, { value: '', isValid: null });
 
   const authCtx = useContext(AuthContext);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   useEffect(() => {
     console.log('EFFECT RUNNING');
@@ -99,15 +102,21 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      emailInputRef.current.focus();
+    } else {
+      passwordInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <Input id="email" label="E-Mail" type="email" isValid={emailIsValid} value={emailState.value} onChange={emailChangeHandler} onBlur={validateEmailHandler} />
+        <Input ref={emailInputRef} id="email" label="E-Mail" type="email" isValid={emailIsValid} value={emailState.value} onChange={emailChangeHandler} onBlur={validateEmailHandler} />
 
-        <Input id="password" label="Password" type="password" isValid={passwordIsValid} value={passwordState.value} onChange={passwordChangeHandler} onBlur={validatePasswordHandler} />
+        <Input ref={passwordInputRef} id="password" label="Password" type="password" isValid={passwordIsValid} value={passwordState.value} onChange={passwordChangeHandler} onBlur={validatePasswordHandler} />
 
         {/* Replaced by custom component above */}
         {/* <div className={`${classes.control} ${passwordState.isValid === false ? classes.invalid : ''}`}>
@@ -116,7 +125,7 @@ const Login = (props) => {
         </div> */}
 
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useRef, useImperativeHandle } from 'react';
 
 import classes from './Input.module.css';
 
-const Input = (props) => {
+// if ref is set from parent, then we can access ref
+// forwardRef needs to be used to enable the second parameter (the ref parameter)
+// forwardRef returns a react component that is cable of being bound to a ref
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  // activate will be called from outside the input
+  const activate = () => {
+    inputRef.current.focus();
+  };
+
+  // make a function or value available to the parent elm through refs
+  useImperativeHandle(ref, () => {
+    return {
+      // accesible to the outside/parent
+      focus: activate,
+    };
+  });
+
   return (
     <div className={`${classes.control} ${props.isValid === false ? classes.invalid : ''}`}>
       <label htmlFor={props.id}>{props.label}</label>
-      <input type={props.type} id={props.id} value={props.value} onChange={props.onChange} onBlur={props.onBlur} />
+      <input ref={inputRef} type={props.type} id={props.id} value={props.value} onChange={props.onChange} onBlur={props.onBlur} />
     </div>
   );
-};
+});
 
 export default Input;
